03ab72d045237e4d5f9671c583271b74b3c3b5bb
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2015-12-05 20:54:51 +0100

    makes ogg_gen.sh more generic

--- ogg_gen.sh
+++ ogg_gen.sh
@@ -3,7 +3,7 @@
 # creates a title.ogg file
 arg="`pwd`/$1"
 cmd='gabctk.py'
-path='/home/lgd/dev/gabctk'  # where you cloned https://github.com/jperon/gabctk
+path="$HOME/gabctk"  # where you cloned https://github.com/jperon/gabctk
 pushd "$path"
 ./"$cmd" -i "$arg" -o - | timidity - -Ov -o "${arg%.gabc}.ogg"
 popd
