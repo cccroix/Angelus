20c835b38c9ddad7501d46365d7fc632aec2a8d9
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2015-12-05 17:02:13 +0100

    améliore le script d’installation
    (ajoute EB Garamond aux dépendances)

--- fedora_install_gregorio.sh
+++ fedora_install_gregorio.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
 pushd ~
-sudo dnf install gcc texlive-luatex flex bison libxml2-devel libtool-ltdl-devel autoconf m4 automake fontforge
+sudo dnf install gcc texlive-luatex texlive-lualibs texlive-ebgaramond flex bison libxml2-devel libtool-ltdl-devel autoconf m4 automake fontforge
+mkluatexfontdb
 wget 'https://github.com/gregorio-project/gregorio/releases/download/v3.0.3/gregorio-3.0.3.tar.gz'
 gunzip gregorio-*.gz
 tar xvf gregorio-*.tar
