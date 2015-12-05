08464096092d3d39bdf85ada8428b695b6879550
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2015-12-05 16:41:48 +0100

    ajoute un script d’installation pour fedora

---
+++ fedora_install_gregorio.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+pushd ~
+sudo dnf install gcc texlive-luatex flex bison libxml2-devel libtool-ltdl-devel autoconf m4 automake fontforge
+wget 'https://github.com/gregorio-project/gregorio/releases/download/v3.0.3/gregorio-3.0.3.tar.gz'
+gunzip gregorio-*.gz
+tar xvf gregorio-*.tar
+pushd gregorio-*
+./build.sh && sudo ./install.sh
+popd
+popd
+exit 0;
